7f01f0c0db42c982b266248084a0e64209f9dc89
Magnus Lund <mgnusl@gmail.com>
2014-10-01 15:30:18 +0200

    Only admin can delete users closes #7

--- src/webapp/controllers/AdminController.php
+++ src/webapp/controllers/AdminController.php
@@ -32,10 +32,21 @@ class AdminController extends Controller
 
     function delete($username)
     {
+        if (Auth::guest()) {
+            $this->app->flash('info', "You must be logged in to view the admin page.");
+            $this->app->redirect('/');
+        }
+
+        if (! Auth::isAdmin()) {
+            $this->app->flash('info', "You must be administrator to view the admin page.");
+            $this->app->redirect('/');
+        }
+
+
         if (User::deleteByUsername($username) === 1) {
-            $this->app->flash('info', "Sucessfully deleted '$username'");
+            $this->app->flash('info', "Successfully deleted '$username'");
         } else {
-            $this->app->flash('info', "An error ocurred. Unable to delete user '$username'.");
+            $this->app->flash('info', "An error occurred. Unable to delete user '$username'.");
         }
 
         $this->app->redirect('/admin');
